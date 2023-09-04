591b13f7ab422ae8d74cbca9f947b6b38c46cc3b
Deb-Deep-Dutta <123333564+Deb-Deep-Dutta@users.noreply.github.com>
2023-09-04 21:11:13 +0530

    Update script.sql

--- DBMS/Week 4/script.sql
+++ DBMS/Week 4/script.sql
@@ -1,8 +1,8 @@
---1. Show the use of upper and lower function.
+/*1. Show the use of upper and lower function.*/
 
 select upper('hello world'), lower('HOW ARE YOU') from dual
 
---2. Show the use of concat, instr and length function
+/*2. Show the use of concat, instr and length function*/
   
 select concat('Hello ','World') as "Concatenated String" from dual;
 select instr('Hello World','o') as "First Occurance" from dual;
@@ -38,13 +38,13 @@ c. Calculate 8^7.*/
 
 select ceil(14.887) as "Ceiling", floor(14.887) as "Floor", round(17.49989) as "Round", power(8,7) as "Power" from dual;
 
---5. Show the current date
+/*5. Show the current date*/
 
 select to_char(current_date,'dd-mm-yyyy') as "Current Date" from dual;
---or select current_date as "Current Date" from dual;
---or select sysdate as "Current Date" from dual;
+/*or select current_date as "Current Date" from dual;
+or select sysdate as "Current Date" from dual;*/
 
---6. Find the total experience of the employees in weeks who works in Sales department
+/*6. Find the total experience of the employees in weeks who works in Sales department*/
 
 select emp_id, f_name, l_name, round((to_date(sysdate,'dd-mon-yyyy')-to_date(date_of_joining,'dd-mon-yyyy'))/7) as "Experience in Weeks" from employee where dept='Sales' order by emp_id;
 
@@ -53,24 +53,24 @@ a. Months_between*/
 
 select months_between(to_date(sysdate,'dd-mm-yyyy'),to_date('20-04-2003','dd-mm-yyyy')) as "Month Difference" from dual;
 
---b. Add_months
+/*b. Add_months*/
 
 select add_months(sysdate,3) as "Months" from dual;
 
---c. Next_day
+/*c. Next_day*/
 
 select next_day(sysdate,4) as "Day" from dual;
 
---d. Last_day
+/*d. Last_day*/
 
 select last_day(sysdate) as "Last" from dual;
 
---e. Round
+/*e. Round*/
 
 select round(sysdate) as "Rounded" from dual;
 
---f. Trunc
+/*f. Trunc*/
 select trunc(systimestamp,'y') as "Truncated" from dual;
 
---g. To_char
+/*g. To_char*/
 select to_char(systimestamp,'DD-MON-YYYY HH24:MI:SS TZH:TZM') as "Formatted Date" from dual;
